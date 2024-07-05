9a3c383c56005797dff49d45f5832ac5b394a90e
TEO <alfredo.ssm@gmail.com>
2024-07-05 10:45:44 -0400

    Refactorizacion de codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
-const getIndicadores = async () => {
+const renderMessage = (messageText, className, container) => {
+  container.innerHTML = ''
   const message = document.createElement('p')
+  message.className = className
+  message.textContent = messageText
+  container.appendChild(message)
+
+}
+
+const getIndicadores = async () => {
   try {
     const resp = await fetch('https://mindicador.cl/api')
     const data = await resp.json()
     return data
   } catch (error) {
-    message.className = 'text-center text-red-500 font-bold'
-    message.textContent = 'Error en la petición intente nuevamente.'
+    renderMessage('Error en la petición intente nuevamente.', errorClasses, result)
+    throw error
   }
 }
 
@@ -16,30 +24,28 @@ const getCurrencyValue = async (currency) => {
   switch (currency) {
     case 'USD':
       exchangeRate = data.dolar.valor
-      console.log({ exchangeRate });
       break;
     case 'EUR':
       exchangeRate = data.euro.valor
-      console.log({ exchangeRate });
       break;
+    default:
+      renderMessage('Tipo de moneda no soportado.', errorClasses, result)
+      throw new Error('Tipo de moneda no soportado.')
   }
 
   return exchangeRate
 }
 
 const convertCurrency = async (amount, currency) => {
-  console.log('amount', amount);
-  console.log('currency', currency);
   const exchangeRate = await getCurrencyValue(currency)
-  console.log('exchangeRate', exchangeRate);
   const convertedAmount = amount / exchangeRate
   return convertedAmount
 }
 
 const currency_types = ['USD', 'EUR']
+const errorClasses = 'text-center text-red-500 font-bold'
 
 const selectCurrency = document.querySelector('#currency')
-
 currency_types.forEach(currency => {
   const option = document.createElement('option')
   option.value = currency
@@ -55,26 +61,20 @@ const updateBtnState = () => {
   const amountNumber = inputAmount.value * 1
   if (selectCurrency.value !== 'Seleccione moneda' && (amountNumber > 0)) {
     btnBuscar.disabled = false
-    console.log('true', btnBuscar.disabled);
   } else {
     btnBuscar.disabled = true
-    console.log('galse', btnBuscar.disabled);
   }
+}
+
+const handleConversion = async () => {
+  const amount = inputAmount.value * 1
+  const currency = selectCurrency.value
+
+  const convertedAmount = await convertCurrency(amount, currency)
+
+  renderMessage(`Resultado: ${convertedAmount.toFixed(2)}`, 'text-center text-white font-bold', result)
 }
 
 selectCurrency.addEventListener('change', updateBtnState)
 inputAmount.addEventListener('input', updateBtnState)
-btnBuscar.addEventListener('click', async () => {
-  const amount = inputAmount.value * 1
-  console.log('amountamount', amount);
-  const currency = selectCurrency.value
-
-  result.innerHTML = ''
-
-  const convertedAmount = await convertCurrency(amount, currency)
-  const message = document.createElement('p')
-  message.className = 'text-center text-white font-bold'
-  message.textContent = `Resultado: ${convertedAmount.toFixed(2)}`
-  result.appendChild(message)
-  console.log('convertedAmount', convertedAmount);
-})
+btnBuscar.addEventListener('click', handleConversion)
