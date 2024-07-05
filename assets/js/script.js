78d570cd161ca01b97bd502359d91087d64303a7
TEO <alfredo.ssm@gmail.com>
2024-07-04 22:58:42 -0400

    Se carga de tipos de monedas el select y se crean validaciones para activar boton buscar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log('Hola')
+const currency_types = ['CLP', 'USD', 'EUR']
+
+const selectCurrency = document.querySelector('#currency')
+
+currency_types.forEach(currency => {
+  const option = document.createElement('option')
+  option.value = currency
+  option.textContent = currency
+  selectCurrency.appendChild(option)
+})
+
+const btnBuscar = document.querySelector('#convert')
+const inputAmount = document.querySelector('#amount')
+
+const updateBtnState = () => {
+  const amountNumber = inputAmount.value * 1
+  if (selectCurrency.value !== 'Seleccione moneda' && (amountNumber > 0)) {
+    btnBuscar.disabled = false
+    console.log('true', btnBuscar.disabled);
+  } else {
+    btnBuscar.disabled = true
+    console.log('galse', btnBuscar.disabled);
+  }
+}
+
+selectCurrency.addEventListener('change', updateBtnState)
+inputAmount.addEventListener('input', updateBtnState)
