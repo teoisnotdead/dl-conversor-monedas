2234e0e87c472e45203eb185f35ce5031e7f461f
TEO <alfredo.ssm@gmail.com>
2024-07-05 00:15:20 -0400

    Se deja flujo completo de conversor de monedas

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,38 @@ const getIndicadores = async () => {
   try {
     const resp = await fetch('https://mindicador.cl/api')
     const data = await resp.json()
-    console.log(data);
-    const resultado = data.dolar.valor
-    message.className = 'text-center text-green-500 font-bold'
-    message.textContent = `Resultado: ${resultado}`
+    return data
   } catch (error) {
-    console.log('error', error);
     message.className = 'text-center text-red-500 font-bold'
     message.textContent = 'Error en la petición intente nuevamente.'
   }
-  result.appendChild(message)
 }
 
-getIndicadores()
+const getCurrencyValue = async (currency) => {
+  const data = await getIndicadores()
+  let exchangeRate
+  switch (currency) {
+    case 'USD':
+      exchangeRate = data.dolar.valor
+      console.log({ exchangeRate });
+      break;
+    case 'EUR':
+      exchangeRate = data.euro.valor
+      console.log({ exchangeRate });
+      break;
+  }
+
+  return exchangeRate
+}
+
+const convertCurrency = async (amount, currency) => {
+  console.log('amount', amount);
+  console.log('currency', currency);
+  const exchangeRate = await getCurrencyValue(currency)
+  console.log('exchangeRate', exchangeRate);
+  const convertedAmount = amount / exchangeRate
+  return convertedAmount
+}
 
 const currency_types = ['USD', 'EUR']
 
@@ -45,3 +64,17 @@ const updateBtnState = () => {
 
 selectCurrency.addEventListener('change', updateBtnState)
 inputAmount.addEventListener('input', updateBtnState)
+btnBuscar.addEventListener('click', async () => {
+  const amount = inputAmount.value * 1
+  console.log('amountamount', amount);
+  const currency = selectCurrency.value
+
+  result.innerHTML = ''
+
+  const convertedAmount = await convertCurrency(amount, currency)
+  const message = document.createElement('p')
+  message.className = 'text-center text-white font-bold'
+  message.textContent = `Resultado: ${convertedAmount.toFixed(2)}`
+  result.appendChild(message)
+  console.log('convertedAmount', convertedAmount);
+})
